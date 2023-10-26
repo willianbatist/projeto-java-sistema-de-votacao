0e2775499ceb0310d97047f7d5ffc78ec6c6444f
Willian Batista <willian.alves.b15@gmail.com>
2023-10-26 19:55:03 -0300

    feat: class Pessoa finalizado

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,30 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+/**
+ * The type Pessoa.
+ */
+public abstract class Pessoa {
+
+  /**
+   * The Nome.
+   */
+  protected String nome;
+
+  /**
+   * Gets nome.
+   *
+   * @return the nome
+   */
+  public String getNome() {
+    return this.nome;
+  }
+
+  /**
+   * Sets nome.
+   *
+   * @param nome the nome
+   */
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
