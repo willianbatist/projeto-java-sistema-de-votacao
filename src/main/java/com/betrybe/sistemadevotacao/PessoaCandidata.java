3a93498bde06cbee7c0c21c6532e2ba262e82e28
Willian Batista <willian.alves.b15@gmail.com>
2023-10-26 22:05:32 -0300

    feat: PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -3,6 +3,54 @@ package com.betrybe.sistemadevotacao;
 /**
  * The type Pessoa candidata.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
 
+  private int numero;
+  private int votos;
+
+  /**
+   * Instantiates a new Pessoa candidata.
+   *
+   * @param nome   the nome
+   * @param numero the numero
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  /**
+   * Gets votos.
+   *
+   * @return the votos
+   */
+  public int getVotos() {
+    return votos;
+  }
+
+  /**
+   * Gets numero.
+   *
+   * @return the numero
+   */
+  public int getNumero() {
+    return numero;
+  }
+
+  /**
+   * Sets numero.
+   *
+   * @param numero the numero
+   */
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  /**
+   * Receber voto.
+   */
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
