7387536a2204bfb19dbc32f347b47e4faac1fc18
Willian Batista <willian.alves.b15@gmail.com>
2023-10-26 22:49:58 -0300

    feat: GerenciamentoVotacao atributos adicionados

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,8 +1,42 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.ArrayList;
+
 /**
  * The type Gerenciamento votacao.
  */
-public class GerenciamentoVotacao {
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
+  private ArrayList<String> cpfsComputados;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+
+  /**
+   * Instantiates a new Gerenciamento votacao.
+   */
+  public GerenciamentoVotacao() {
+    cpfsComputados = new ArrayList<>();
+    pessoasEleitoras = new ArrayList<>();
+    pessoasCandidatas = new ArrayList<>();
+  }
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
