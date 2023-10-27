6a292112b7577b4d88025921fddf4f97696e511e
Willian Batista <willian.alves.b15@gmail.com>
2023-10-26 22:11:36 -0300

    feat: PessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -3,6 +3,36 @@ package com.betrybe.sistemadevotacao;
 /**
  * The type Pessoa eleitora.
  */
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  /**
+   * Instantiates a new Pessoa eleitora.
+   *
+   * @param nome the nome
+   * @param cpf  the cpf
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  /**
+   * Gets cpf.
+   *
+   * @return the cpf
+   */
+  public String getCpf() {
+    return cpf;
+  }
+
+  /**
+   * Sets cpf.
+   *
+   * @param cpf the cpf
+   */
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
